4a8df4bab37bb2ad88fc217517e058f776b100bf
Alexis Mansilla Ruiz <alexis.mansilla04@gmail.com>
2021-07-31 23:03:32 -0400

    Add useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Snackbar } from '@material-ui/core';
 import Header from './components/Header';
 import SendTweet from './components/SendTweet';
 
+import { TWEETS_STORAGE } from './utils/contants';
+
 function App() {
   const [toastProps, setToastProps] = useState({ open: false, text: null});
+
+  const [allTweets, setAllTweets] = useState([]);
+
+  useEffect(() => {
+    const allTweetsStorage = localStorage.getItem(TWEETS_STORAGE);
+    const allTweetsArray = JSON.parse(allTweetsStorage);
+    setAllTweets(allTweetsArray);
+  }, []);
+
+  console.log(allTweets);
 
   return (
     <div className="App">
